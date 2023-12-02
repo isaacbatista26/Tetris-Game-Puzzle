de12a5191588853b22db9dbb2b3544e3b696eb41
Rafael Carvalho <rrafaelcarvalhoj@gmail.com>
2023-12-02 17:41:48 -0300

    feat: running application with docker

--- frontend/src/Components/TetrisGame/styles/Main.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Main = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  padding-top:18vh;
-  `;
--- frontend/src/Components/TetrisGame/styles/StyledSection.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const StyledSection = styled.div`
-    display: flex;
-    width: 100%;
-    align-items: center;
-    justify-content: center;
-`
