77f6971ef87e7e5f2781c61e567ad9d64a32e71f
Rafael Carvalho <rrafaelcarvalhoj@gmail.com>
2023-12-02 14:23:27 -0300

    fix: ident

--- tetris/src/App.js
+++ tetris/src/App.js
@@ -1,17 +1,16 @@
-// App.js
 import React from 'react';
-
-//import { StyledSection, Main, LeftSide, RightSide } from 'Styles';
+//Styled Components
 import {StyledSection} from './Components/styles/StyledSection';
 import {Main} from './Components/styles/Main';
 import {LeftSide} from './Components/styles/LeftSide';
 import {RightSide} from './Components/styles/RightSide';
+import StyledIcon from './Components/styles/StyledIcon';
+//Components
 import Grid from './Components/Grid';
 import PlayerInfo from './Components/PlayerInfo';
 import GameInfo from './Components/GameInfo'
 import NextTetromino from './Components/NextTetromino'
-import StyledIcon from './Components/styles/StyledIcon';
-
+//Hooks
 import useGridState from './Hooks/useGridState';
 
 const App = ({ playerInfo }) => {
--- tetris/src/Components/styles/SyledGameInfo.js
+++ tetris/src/Components/styles/SyledGameInfo.js
@@ -2,9 +2,8 @@ import styled from "styled-components";
 
 export const StyledGameInfo = styled.div`
     display: block;
-    padding: 0px 30px 10px 10px;
+    padding: 0px 10px 10px 10px;
     width: 100px;
     justfy-content: end;
-    padding-top: 10px;
     border-right: 2px solid #2E2E2E;
 `;
