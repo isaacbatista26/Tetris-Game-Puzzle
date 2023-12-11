be37c7423824beb99c0b3158d99561c1c054a2ba
Rafael Carvalho <rrafaelcarvalhoj@gmail.com>
2023-12-11 10:58:16 -0300

    feat: Building the client and server container

--- client/src/Pages/Tetris.jsx
+++ client/src/Pages/Tetris.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 // npm i react-router-dom
 
 //Styled Components
-import {StyledSection} from './Components/TetrisGame/styles/StyledSection';
-import {Main} from './Components/TetrisGame/styles/Main';
-import {LeftSide} from './Components/TetrisGame/styles/LeftSide';
-import {RightSide} from './Components/TetrisGame/styles/RightSide';
-import StyledIcon from './Components/TetrisGame/styles/StyledIcon';
+import {StyledSection} from '../Components/TetrisGame/styles/StyledSection';
+import {Main} from '../Components/TetrisGame/styles/Main';
+import {LeftSide} from '../Components/TetrisGame/styles/LeftSide';
+import {RightSide} from '../Components/TetrisGame/styles/RightSide';
+import StyledIcon from '../Components/TetrisGame/styles/StyledIcon';
 
 //Components
-import Grid from './Components/TetrisGame/Grid';
-import PlayerInfo from './Components/TetrisGame/PlayerInfo';
-import GameInfo from './Components/TetrisGame/GameInfo'
-import NextTetromino from './Components/TetrisGame/NextTetromino'
+import Grid from '../Components/TetrisGame/Grid';
+import PlayerInfo from '../Components/TetrisGame/PlayerInfo';
+import GameInfo from '../Components/TetrisGame/GameInfo'
+import NextTetromino from '../Components/TetrisGame/NextTetromino'
 
 //Hooks
-import useGridState from './Hooks/useGridState';
+import useGridState from '../Hooks/useGridState';
 
 const App = ({ playerInfo }) => {
   const { grid } = useGridState();
