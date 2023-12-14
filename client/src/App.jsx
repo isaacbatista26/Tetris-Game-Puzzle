08f720009f5bcb99e5b75602482a109fc4292105
Rafael Carvalho J. Araujo <rrafaelcarvalhoj@gmail.com>
2023-12-14 15:09:32 -0300

    feat: React Router Working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
-//import { BrowserRouter , Route, Routes } from 'react-router-dom'
+import { BrowserRouter , Route, Routes } from 'react-router-dom'
 
 import Tetris from './Pages/Tetris';
 
 const App = ({ playerInfo }) => {
   return (
-    /*
     <BrowserRouter>
-    <Routes>
-    <Route path="/" element={<Tetris playerInfo={playerInfo} />} />
-    </Routes>
-    </BrowserRouter>
-    */
-   <Tetris playerInfo={playerInfo}/>
+      <Routes>
+        <Route path="/tetris" element={<Tetris playerInfo={playerInfo} />} />
+      </Routes>
+    </BrowserRouter> 
   );
 };
 
