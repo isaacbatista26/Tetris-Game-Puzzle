99755006de6dd13768ffae7fa65118a630838111
Rafael Carvalho J. Araujo <rrafaelcarvalhoj@gmail.com>
2023-12-16 22:09:09 -0300

    feat: logout button

--- client/src/Components/TetrisGame/Button.jsx
+++ client/src/Components/TetrisGame/Button.jsx
@@ -4,22 +4,22 @@ import styled from 'styled-components';
 const StyledButton = styled.button`
   box-sizing: border-box;
 
-  margin: 0 0 20px 0;
-  padding: 20px;
+  margin: 0 0 15px 15px;
+  padding: 15px;
   min-height: 30px;
-  width: 100%;
-  border-radius: 20px;
+  width: 150px;
+  border-radius: 10px;
   border: none;
   color: white;
   background: #333;
-  font-family: Pixel, Arial, Helvetica, sans-serif;
-  font-size: 1rem;
+  font-family: 'Montserrat', sans-serif;
+  font-size: 15px;
   outline: none;
   cursor: pointer;
 `;
 
-const Button = ({ callback }) => (
-  <StyledButton onClick={callback}>Start Game</StyledButton>
+const Button = ({ callback, label }) => (
+  <StyledButton onClick={callback}>{label}</StyledButton>
 );
 
 export default Button;
